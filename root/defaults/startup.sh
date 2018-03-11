8a323d031e932bf2b2c47d1ca3bd94993a56a33f
oznu <dev@oz.nu>
2018-03-11 20:48:05 +1100

    updated formatting

--- root/defaults/startup.sh
+++ root/defaults/startup.sh
@@ -6,10 +6,16 @@
 # This script can be used to customise the environment and will be executed as
 # the root user each time the container starts.
 #
-# If using this script to install Homebridge plugins please take note:
-#   * DO NOT install plugins using the global flag (-g)
+# If using this to install plugins DO NOT use the global flag (-g).
 #
-# Example:
+# Example installing homebridge plugin:
 #
 # npm install homebridge-hue
 #
+# Example installing packages using default image (Alpine Linux):
+#
+# apk add --no-cache ffmpeg ffmpeg-libs libpcap-dev
+#
+# If you're running a debian based version of this container please use apt-get to install packages.
+#
+
