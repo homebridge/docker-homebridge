775a062d8a309c9e3ffd5fb62053c0262db8bde9
oznu <dev@oz.nu>
2022-05-14 12:29:08 +1000

    remove extra file

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-RELEASE_NAME="some-other"
-
-TARGET_IMAGE_TAG=$(if [ "${{ steps.get_branch.outputs.NAME }}" = "master" ]; then echo "latest"; else echo "${{ steps.get_branch.outputs.NAME }}"; fi;)
-
-echo $TARGET_IMAGE_TAG
