768355fdb9766bdd6a7cd6ea1db8669de7f6b9a0
oznu <dev@oz.nu>
2022-06-16 17:53:07 +1000

    fix #449 - continue to load homebridge if user startup script errors

--- rootfs/etc/s6-overlay/scripts/userdata.sh
+++ rootfs/etc/s6-overlay/scripts/userdata.sh
@@ -9,7 +9,7 @@ if [ -f /homebridge/startup.sh ]; then
   echo "Executing user startup script /homebridge/startup.sh"
   chmod +x /homebridge/startup.sh
   cd /homebridge
-  exec /homebridge/startup.sh
+  /homebridge/startup.sh
 fi
 
 exit 0
